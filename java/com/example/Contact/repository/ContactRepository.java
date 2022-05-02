be7df998c2330add28c9f18105d41e88c017f5c7
PATX0017 <99271854+PATX0017@users.noreply.github.com>
2022-05-02 11:16:20 -0700

    Delete java/com/example/Contact directory

--- java/com/example/Contact/repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Contact.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Contact.model.ContactClass;
-
-public interface ContactRepository extends JpaRepository<ContactClass, Long> {
-
-}
